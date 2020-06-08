60045893d0a77ac95309f578dbf31a01e18a6175
dpdlzpdl21 <63216361+dpdlzpdl21@users.noreply.github.com>
2020-06-08 15:41:28 +0900

    30107박준아

---
+++ 30107박준아(군집데이터).py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## [군집데이터]
+
+# In[28]:
+
+
+True==False
+False==0
+
+
+# In[4]:
+
+
+eng = 'kkk'
+big_eng = eng.upper()
+big_eng
+
+
+# In[11]:
+
+
+data=[]
+data.append('박준아')
+data.append(1)
+data[:2]
+
+
+# In[20]:
+
+
+ban = {'박준아','지유민'}
+num = {2,3,4,'박준아'}
+ban |num
+
+
+# In[21]:
+
+
+ban&num
+
+
+# In[ ]:
+
+
+
+
