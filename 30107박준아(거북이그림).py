7d4990bd29a7c702a32415b80936497a899a5307
dpdlzpdl21 <63216361+dpdlzpdl21@users.noreply.github.com>
2020-06-12 12:03:50 +0900

    30107박준아(거북이그림  과제)

---
+++ 30107박준아(거북이그림).py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+t = turtle.Pen()
+ 
+t.right(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.right(180-36)
+t.forward(100)
+t.left(180-108)
+t.forward(100)
+t.right(180-36)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.right(180-36)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.right(180-36)
+t.forward(100)
+t.left(72)
+t.forward(100)
+
+
+# In[4]:
+
+
+import turtle as t
+t.shape('turtle')
+t.color('blue','green')
+t.begin_fill()
+t.exltonclick()
+
+
+# In[2]:
+
+
+t.shape('turtle')
+t.color('blue','green')
+t.begin_fill()
+
+n = int(input('몇각형??? '))
+
+for i in range(n):
+    t.fd(100)
+    t.left(360/n)
+t.end_fill()
+t.exitonclick()
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+t.shape('turtle')
+t.color('blue','green')
+t.beain_fill()
+
+n = int(input('몇각형???'))
+
+for i in range(n)
+    for i in range(n)
+    t.fd(100)
+    
+
+
+# In[2]:
+
+
+import turtle as t
+ 
+t.shape('turtle')
+t.speed('fastest')      # 거북이 속도를 가장 빠르게 설정
+for i in range(300):    # 300번 반복
+    t.forward(i)        # i만큼 앞으로 이동. 반복할 때마다 선이 길어짐
+    t.right(91) 
+
+
+# In[ ]:
+
+
+
+
